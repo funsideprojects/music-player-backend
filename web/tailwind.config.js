e3e87cd8046d5b69ccf145e8fa6277711549f872
Phat <tienphat1501@gmail.com>
2021-03-28 23:57:29 +0700

    update spacing tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
     container: {
       center: true,
     },
+    spacing: {
+      '0': '0px',
+      '4': '4px',
+      '8': '8px',
+      '12': '12px',
+      '16': '16px',
+      '20': '20px',
+      '24': '24px',
+      '28': '28px',
+      '32': '32px',
+      '36': '36px',
+      '40': '40px',
+    },
     extend: {},
   },
   variants: {
